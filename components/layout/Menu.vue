33cea85a471f8925157350d2e34ad5725fe5b858
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-07-27 16:03:33 +0300

    added sidebar menu

---
+++ components/layout/Menu.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {MENU_DATA} from "./menu.data";
+</script>
+
+<template>
+  <div>
+    <NuxtLink
+        v-for="item in MENU_DATA"
+        class="flex items-center py-1 px-3 w-full mb-2 transition-all hover:text-active menu-link"
+        :key="item.name"
+        :to="item.url"
+    >
+      <Icon :name="item.icon" class="mr-3"/>
+      <span>{{ item.name }}</span>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.menu-link:last-child {
+  @apply border-t-2;
+}
+</style>
