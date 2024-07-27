8239990d4abe66aad7cb040e12fa8aaadfe22715
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-07-27 14:35:22 +0300

    added fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,22 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
-    modules: ["@nuxt/image", '@nuxtjs/tailwindcss', "shadcn-nuxt"],
+    modules: [
+        "@nuxt/image",
+        '@nuxtjs/tailwindcss',
+        "shadcn-nuxt",
+        "@nuxtjs/google-fonts"
+    ],
     shadcn: {
         prefix: 'ui',
         componentDir: './components/ui'
+    },
+    googleFonts: {
+        families: {
+            Lato: {
+                wght: [300, 400, 700],
+                ital: [300]
+            }
+        }
     }
 })
