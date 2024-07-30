0d029947d86b0e2b7ee4b19c2773d000c2946292
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-07-30 19:48:07 +0300

    transformed dashboard data

--- components/kanban/useKanbanQuery.ts
+++ components/kanban/useKanbanQuery.ts
@@ -3,6 +3,7 @@ import {DB} from "~/lib/appwrite";
 import {COLLECTION_DEALS, DB_ID} from "~/app.constants";
 import {KANBAN_DATA} from "~/components/kanban/kanban.data";
 import type {IDeal} from "~/types/deals.types";
+import type {IColumn} from "~/components/kanban/kanban.types";
 
 export function useKanbanQuery() {
     return useQuery({
@@ -10,8 +11,27 @@ export function useKanbanQuery() {
         queryFn: () => DB.listDocuments(DB_ID, COLLECTION_DEALS),
         select(data) {
             const newBoard = [...KANBAN_DATA]
+            const newBoardMap: Map<string, IColumn> = new Map();
+            for (const column of newBoard) {
+                newBoardMap.set(column.id, column);
+            }
+
             const deals = data.documents as unknown as IDeal[];
-            debugger;
+            for (const deal of deals) {
+                const column = newBoardMap.get(deal.status);
+                if (!column) {
+                    continue;
+                }
+
+                column.items.push({
+                    $createdAt: deal.$createdAt,
+                    id: deal.$id,
+                    name: deal.name,
+                    price: deal.price,
+                    companyName: deal.customer.name,
+                    status: deal.status
+                });
+            }
         }
     });
 }
