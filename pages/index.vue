3a8e1b747afa81bbb17c5d59ccf4769e75976092
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-07-30 13:54:33 +0300

    added dashboard card

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,16 @@
 </script>
 
 <template>
-<div class="p-10">
-  <h1 class="font-bold text-2xl mb-10">Vue CRM</h1>
-</div>
+  <div class="p-10">
+    <h1 class="font-bold text-2xl mb-10">Vue CRM</h1>
+    <div>
+      <UiCard class="mb-3" draggable="true">
+        <UiCardHeader role="button">Name card</UiCardHeader>
+        <UiCardContent>Company</UiCardContent>
+        <UiCardFooter>Date</UiCardFooter>
+      </UiCard>
+    </div>
+  </div>
 </template>
 
 <style scoped>
