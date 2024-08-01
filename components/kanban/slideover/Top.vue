367692e37054f69e46ae4662d052f2288a81f269
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-02 00:53:04 +0300

    implemented slideover top

--- components/kanban/slideover/Top.vue
+++ components/kanban/slideover/Top.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
+import dayjs from "dayjs";
+import {useDealSlideStore} from "~/store/deal-slide.store";
+import {convertCurrency} from "~/lib/convertCurrency";
+
+const store = useDealSlideStore()
+
 
 </script>
 
 <template>
-<div class="border-border rounded p-3">
-  <div class="uppercase bold text-xl mb-4">About deal</div>
-</div>
+  <div class="border-border rounded p-3">
+    <div class="uppercase bold text-xl mb-4">About deal</div>
+    <KanbanSlideoverLabel label-text="Name">
+      <h2>{{ store.card?.name }}</h2>
+    </KanbanSlideoverLabel>
+    <KanbanSlideoverLabel label-text="Sum">
+      {{ convertCurrency(store.card?.price || 0) }}
+    </KanbanSlideoverLabel>
+    <KanbanSlideoverLabel label-text="Status">
+      <!--  TODO: implement badge    -->
+    </KanbanSlideoverLabel>
+    <KanbanSlideoverLabel label-text="Client">
+      {{ store.card?.companyName }}
+    </KanbanSlideoverLabel>
+    <KanbanSlideoverLabel label-text="Date created">
+      {{ dayjs(store.card?.$createdAt).format('DD MMMM YYYY') }}
+    </KanbanSlideoverLabel>
+  </div>
 </template>
 
 <style scoped>
