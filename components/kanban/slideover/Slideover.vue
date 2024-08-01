74fe9f01bae84a0b7a2541237c94cf021f61bebb
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-02 02:04:34 +0300

    added slide over

--- components/kanban/slideover/Slideover.vue
+++ components/kanban/slideover/Slideover.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import {useDealSlideStore} from "~/store/deal-slide.store";
 
+const dealSlideStore = useDealSlideStore()
 </script>
 
 <template>
-
+<USlideover :model-value="dealSlideStore.isOpen" @update:modelValue="dealSlideStore.isOpen = $event">
+Test
+</USlideover>
 </template>
 
 <style scoped>
