efd84a0ad92c5471687b3ff299624ab41c100f17
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-02 20:45:04 +0300

    updated card styles

--- components/ui/card/Card.vue
+++ components/ui/card/Card.vue
@@ -12,7 +12,7 @@ const props = defineProps<{
     :class="
       cn(
         `rounded-xl bg-card text-card-foreground animation
-        border border-transparent transition-colors hover:border-active p-3`,
+        border border-transparent transition-colors hover:border-slate-500 p-3`,
         props.class,
       )
     "
