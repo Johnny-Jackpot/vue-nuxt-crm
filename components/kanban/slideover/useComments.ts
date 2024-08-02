01215aa5dc42d870b4d52c0fa1737049c6740779
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-02 20:39:40 +0300

    implemented adding comments

---
+++ components/kanban/slideover/useComments.ts
@@ -0,0 +1,14 @@
+import {useDealSlideStore} from "~/store/deal-slide.store";
+import {useQuery} from "@tanstack/vue-query";
+import {DB} from "~/lib/appwrite";
+import {COLLECTION_DEALS, DB_ID} from "~/app.constants";
+
+export function useComments() {
+    const dealSlideStore = useDealSlideStore()
+    const cardId = dealSlideStore.card?.id || ''
+
+    return useQuery({
+        queryKey: ['deal', cardId],
+        queryFn: () => DB.getDocument(DB_ID, COLLECTION_DEALS, cardId)
+    })
+}
