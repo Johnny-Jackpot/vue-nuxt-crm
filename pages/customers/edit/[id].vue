717ee9f57fda6b207b4e1eab6440c6307aebf835
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-02 23:12:54 +0300

    edit customer logic

--- pages/customers/edit/[id].vue
+++ pages/customers/edit/[id].vue
@@ -29,8 +29,8 @@ const {data, isSuccess} = useQuery({
   queryFn: () => DB.getDocument(DB_ID, COLLECTION_CUSTOMERS, customerId),
 })
 
-watchEffect(() => {
-  const initialData = data as unknown as ICustomerFormState
+watch(data, (newData) => {
+  const initialData = newData as unknown as ICustomerFormState
   setValues({
     email: initialData.email,
     from_source: initialData.from_source || '',
@@ -44,23 +44,53 @@ const fieldConfig = {
   })
 }
 
-const [name, nameAttr] = defineField('name', fieldConfig)
-const [email, emailAttr] = defineField('email', fieldConfig)
-const [fromSource, fromSourceAttr] = defineField('from_source', fieldConfig)
+const [name, nameAttrs] = defineField('name', fieldConfig)
+const [email, emailAttrs] = defineField('email', fieldConfig)
+const [fromSource, fromSourceAttrs] = defineField('from_source', fieldConfig)
 
 const {mutate, isPending} = useMutation({
   mutationKey: ['update customer', customerId],
   mutationFn: (data: ICustomerFormState) => DB.updateDocument(DB_ID, COLLECTION_CUSTOMERS, customerId, data)
 })
 
-const unSubmit = handleSubmit((values) => {
+const onSubmit = handleSubmit((values) => {
   mutate(values)
 })
 
 </script>
 
 <template>
-
+  <div class="md:w-1/2 mx-auto">
+    <h1 class="font-bold text-2xl mb-10 text-center">
+      Edit customer {{ (data as unknown as ICustomerFormState)?.name }}
+    </h1>
+    <form @submit="onSubmit">
+      <UiInput
+          v-model="name"
+          v-bind="nameAttrs"
+          placeholder="Name"
+          type="text"
+          class="my-1"
+      />
+      <UiInput
+          v-model="email"
+          v-bind="emailAttrs"
+          placeholder="Email"
+          type="text"
+          class="my-1"
+      />
+      <UiInput
+          v-model="fromSource"
+          v-bind="fromSourceAttrs"
+          placeholder="Source"
+          type="text"
+          class="my-1"
+      />
+      <UiButton variant="outline" @click="onSubmit" class="my-1" :disable="isPending" type="submit">
+        {{ isPending ? 'Loading...' : 'Update' }}
+      </UiButton>
+    </form>
+  </div>
 </template>
 
 <style scoped>
