1564dba3ef540042aa2d90c35cbeed7b4a534833
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-16 09:51:37 +0300

    added confirm modal

---
+++ components/ui/ConfirmModal.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const props = defineProps({
+  buttonColor: {
+    type: String,
+    default: "green",
+  },
+});
+defineEmits(["confirm"]);
+const isOpen = defineModel<boolean>();
+</script>
+
+<template>
+  <UModal v-model="isOpen">
+    <div class="p-6">
+      <h4 class="text-center mb-4">
+        <slot />
+      </h4>
+      <div class="flex align-middle justify-center gap-2">
+        <UButton
+          size="lg"
+          @click="
+            $emit('confirm');
+            isOpen = !isOpen;
+          "
+          :color="buttonColor"
+          >Confirm
+        </UButton>
+        <UButton
+          size="lg"
+          variant="outline"
+          @click="isOpen = !isOpen"
+          :color="buttonColor"
+          >Cancel
+        </UButton>
+      </div>
+    </div>
+  </UModal>
+</template>
+
+<style scoped></style>
